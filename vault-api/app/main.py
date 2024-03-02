d35879005b3535664d569db20cc17fe61d0addd4
Ali Maharramli <ali.maharramli.std@bhos.edu.az>
2024-03-02 14:01:55 +0400

    add vault client

--- vault-api/app/main.py
+++ vault-api/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import s3_router
 from .auth import check_auth
 
 app = FastAPI(dependencies=[Depends(check_auth)])
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 app.include_router(s3_router.router)
