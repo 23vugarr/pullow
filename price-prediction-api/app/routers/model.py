d176646c03e26d7eff30a4fc02f6fd0afff83fbe
Vugar Abdulali <vabdulaliyev@gmail.com>
2024-03-02 12:33:52 +0400

    model api ready

--- price-prediction-api/app/routers/model.py
+++ price-prediction-api/app/routers/model.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security.api_key import APIKey
 
 from app.core.auth import get_api_key
@@ -11,12 +11,17 @@ router = APIRouter()
 
 @router.post("/")
 async def test(mlbase: MlBase, api_key: APIKey = Depends(get_api_key)):
-    sq_price = ModelUtily.get_square(price=mlbase.price, city=mlbase.city)
-    future_price = ModelUtily.predict_and_adjust(city=mlbase.city, type_=0, months_to_predict=120)
-    future_price = list(future_price)
-    for index, value in enumerate(future_price):
-        future_price[index] *= sq_price * 1.03
-        future_price[index] = int(future_price[index])
+    try:
+        sq_price = ModelUtily.get_square(price=mlbase.price, city=mlbase.city)
+        future_price = ModelUtily.predict_and_adjust(city=mlbase.city, type_=0, months_to_predict=120)
+        result_map = {}
+        month = 1
 
-    return ResponseBase(data={"result": future_price}, detail=None)
+        for index, _ in enumerate(future_price):
+            if(month > 35):
+                result_map[str(month)] = int(future_price[index] * sq_price * 1.03)
+            month+=1
 
+        return ResponseBase(data={"result": result_map}, detail=None)
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
